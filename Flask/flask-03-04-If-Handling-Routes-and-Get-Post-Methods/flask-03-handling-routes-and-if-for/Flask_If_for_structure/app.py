5c9e142f72538c75a52c8078773c2e93e24f6bda
aydin <aydintokuslu78@gmail.com>
2023-01-20 23:09:52 +0300

    20.01.23

--- Flask/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/Flask_If_for_structure/app.py
+++ Flask/flask-03-04-If-Handling-Routes-and-Get-Post-Methods/flask-03-handling-routes-and-if-for/Flask_If_for_structure/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Create a function named head which shows the massage as "This is my first conditions experience" in `index.html`
 # and assign to the route of ('/')
-
+#
 
 @app.route("/")
 def head():
@@ -24,5 +24,5 @@ def header():
 
 # run this app in debug mode on your local.
 if __name__ == "__main__":
-    # app.run(debug=True)
+    #app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
